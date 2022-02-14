24c830273e7491a15ea42e3cd6a1bc489cbe7921
felo <felix.kurgat@student.moringaschool.com>
2022-02-14 09:27:23 +0300

    added user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,7 +10,7 @@ import { User } from '../user';
 })
 export class UserComponent implements OnInit {
 
-  user!:User;
+  
   userRepo : any;
 
   constructor(private userService: UserService, private repoService : RepoService,private http:HttpClient) {
