e87f0f973143651f406cc4c14ae514e1f3b93fe6
felo <felix.kurgat@student.moringaschool.com>
2022-02-14 12:59:42 +0300

    added chnges

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,6 +3,7 @@ import { UserService } from '../user-service/user.service';
 import { RepoService } from '../repo-service/repo.service';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../user';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -11,10 +12,10 @@ import { User } from '../user';
 export class UserComponent implements OnInit {
 
   user!:User;
-  
+  userRepo : any;
 
   constructor(private userService: UserService, private repoService : RepoService,private http:HttpClient) {
-    this.user = new User('', '', '', '', 0, 0, 0, 0,);
+    this.user = new User('', '', '', '', 0, 0, 0, 0, '');
   }
   // fetching user 
   performSearch(searchTerm:any) {
