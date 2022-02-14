5a483524874b448cf37b1bb8f1bb047a70f0e2f3
felo <felix.kurgat@student.moringaschool.com>
2022-02-14 08:43:38 +0300

    added templateUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Output,EventEmitter } from '@angular/core';
 
 
 @Component({
+  selector: 'app-root',
  
-  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
