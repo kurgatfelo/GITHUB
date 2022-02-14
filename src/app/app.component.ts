c992b432ec2d7ad2eabbecdbc76bea2f4b891e51
felo <felix.kurgat@student.moringaschool.com>
2022-02-14 08:44:21 +0300

    added styleUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  templateUrl: './app.component.html',
  
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'github';
