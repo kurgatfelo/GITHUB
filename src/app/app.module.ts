d84b41c3f3f34e09fb394bd588529d73d3e30e0d
felo <felix.kurgat@student.moringaschool.com>
2022-02-14 13:50:10 +0300

    added changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { UserService } from './user-service/user.service';
+import { RepoService } from './repo-service/repo.service';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { FormsModule } from '@angular/forms';
+import { DateCountPipe } from './date-count.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    RepositoriesComponent,
+    SearchFormComponent,
+    DateCountPipe,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  
+    
+  ],
+  providers: [
+    UserService,
+    RepoService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
